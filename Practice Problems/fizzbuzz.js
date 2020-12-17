f5cf119fe91f9cdda415c20c07d4dd0321b71498
Mike Sanders <71601403+Mike-C-Sanders@users.noreply.github.com>
2020-12-17 07:30:32 -0800

    Added FizzBuzz Solution
    added solution plus a few test cases

--- Practice Problems/fizzbuzz.js
+++ Practice Problems/fizzbuzz.js
@@ -1,1 +1,29 @@
+/**
+ * The Problem.
+ * 
+ * Write a short program that prints each number from 1 to 100 on a new line. 
 
+For each multiple of 3, print "Fizz" instead of the number. 
+
+For each multiple of 5, print "Buzz" instead of the number. 
+
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+ */
+function fizzBuzz(input){
+  
+  for(let i =0; i < input; i++){
+        if(input%3 === 0 && input%5 === 0){
+            console.log('FizzBuzz');
+        }
+        else if(input%5 === 0){
+            console.log('Buzz');
+        }
+        else if(input%3 === 0){
+            console.log('Fizz');
+        }
+  }
+}
+
+fizzBuzz(100);
+
+fizzBuzz(200);
